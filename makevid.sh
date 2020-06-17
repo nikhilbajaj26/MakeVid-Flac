39bcbc4d9232f70173701eca81dcc032b75233ac
Nikhil Bajaj <58053874+nikhilbajaj26@users.noreply.github.com>
2020-06-17 14:12:18 -0400

    Add files via upload
    Added global image resize option in quiet mode, title edit dialog w/ input confirmation, autoselect image option in interactive mode.
    Minor fixes.

--- makevid.sh
+++ makevid.sh
@@ -52,17 +52,45 @@ images="$PWD/$endname"
 namer "tracklists.txt"
 tlists="$PWD/$endname"
 
+[[ $@ =~ [Ii] ]] && global=Interactive
+[[ $@ =~ [Qq] ]] && global=Quiet
 
-echo "Select mode"
-select global in "Interactive" "Quiet"; do [[ $global ]] && break; done
+if [[ ! $global ]]; then
+  echo "Select mode"
+  select global in "Interactive" "Quiet"; do [[ $global ]] && break; done
+fi
+
+if [[ $global =~ ^Q ]]; then
+  resize=0
+  while true; do
+    read -p "Resize images? (y/n)" yn
+    case "$yn" in
+      [Yy] ) resize=1; break;;
+      [Nn] ) break;;
+      * ) echo "Please answer y/Y or n/N.";;
+    esac
+  done
+  if (( resize==1 )); then
+    select opt in "1080p" "720p" "Custom"; do
+      if [[ $opt =~ C ]]; then
+        while [[ ! $res =~ ^[1-9][0-9]*$ ]]; do
+          read -p "Enter custom resolution: " res
+          res="$(echo "$res" | sed -E 's/^0+//')"
+        done
+        break
+      elif [[ $opt ]]; then
+        res="${opt%p}"
+        break
+      fi
+    done
+  fi
+fi
 
 #first define makevid function.
 #Return breaks a function. Exit does the whole script.
 #makevid function as-is just does makevid w/ files in current directory. uses absolute paths, though, so can run anywhere, output will be where you started the fxn. Use this to recurse.
 
 makevid () {
-
-  [[ $global =~ ^I ]] && echo "$PWD"
 
   [[ ! $(echo *.flac) ]] && return
   [[ ! $(echo *.{png,jpg,jpeg}) ]] && return
@@ -81,7 +109,7 @@ makevid () {
     if ! grep -qs '^i' "$prefs"; then #q=quiet,s=noerrors
       printf "\n"
       while true; do
-        read -p "Edit title? Type 'd' to permanently hide this prompt. (y/n/d)" ynd
+        read -p "Edit title? Type 'd' to disable prompt. (y/n/d)" ynd
         case "$ynd" in
           [Yy] ) edit=1; break;;
           [Nn] ) break;;
@@ -93,9 +121,31 @@ makevid () {
     if (( edit==1 )); then
       title=''
       while [[ ! $title ]]; do
+
+        #Read title & sanitize
         echo "Enter new title:"; read title
         title="$(echo "$title" | sed 's@/@-@g')"
+        [[ ! $title ]] && title="$(basename "$PWD")"
+
+        #Print title for confirmation
+        clear; echo "$PWD"
+        table_list=( "$title" )
+        table "TITLE"
+        printf "\n"
+
+        #Confirm or reset
+        while true; do
+          read -p "Confirm title? (y/n)" yn
+          case "$yn" in
+            [Yy] ) break;;
+            [Nn] ) title=''; break;;
+            * ) echo "Please answer y/Y or n/N.";;
+          esac
+        done
+
       done
+
+      #Print end result and continue
       clear; echo "$PWD"
       table_list=( "$title" )
       table "TITLE"
@@ -106,13 +156,20 @@ makevid () {
     for j in *.{png,jpg,jpeg}; do
       for k in {w,h}; do eval "$k=\$(identify -format \"%${k}\" \"$j\")"; done
       list+=( "${j} (${w}x${h})" )
+      grep -qs '^n' "$prefs" && break
     done
     case ${#list[@]} in
       1) img="${list[0]}"
         ;;
       *) printf "\nSelect image\n"
-        select img in "${list[@]}"; do
-          [[ "$img" ]] && break
+        select img in "${list[@]}" "Always autoselect first image."; do
+          if [[ $img = *. ]]; then
+            img="${list[0]}"
+            echo "n" >> "$prefs"
+            break
+          elif [[ $img ]]; then
+            break
+          fi
         done
         ;;
     esac
@@ -125,20 +182,17 @@ makevid () {
     #Image resize
     s=$(grep -s '^s' "$prefs"); res=${s#s}
     if [[ ! $s ]]; then
-      printf "\n" && newline=1
-      #Ask resize
       resize=0
+      printf "\n" && newline=1
       while true; do
-        read -p "Resize image? Type 'd' to permanently hide this prompt. (y/n/d)" ynd
-        case "$ynd" in
+        read -p "Resize image? (y/n)" yn
+        case "$yn" in
           [Yy] ) resize=1; break;;
           [Nn] ) break;;
-          [Dd] ) echo "s" >> "$prefs"; break;;
-          * ) echo "Please answer y/Y, n/N, or d/D.";;
+          * ) echo "Please answer y/Y or n/N.";;
         esac
       done
       if (( resize==1 )); then
-        #Ask res
         select opt in "1080p" "720p" "Custom"; do
           if [[ $opt =~ C ]]; then
             while [[ ! $res =~ ^[1-9][0-9]*$ ]]; do
@@ -151,38 +205,38 @@ makevid () {
             break
           fi
         done
-        #Apply to all y/n
-        while true; do
-          read -p "Apply to all images? (y/n)" yn
-          case "$yn" in
-            [Yy] ) echo "s${res}" >> "$prefs"; break;;
-            [Nn] ) break;;
-            * ) echo "Please answer y/Y or n/N.";;
-          esac
-        done
       fi
-    fi
-
-    #Actual resize, either read from prefs or stored from above
-    if [[ $res ]]; then
-      (( w>h )) && c='^' || c=''
-      namer "${name%.*}.png" #Sets endname to non-taken filename
-      (( newline==0 )) && printf "\n" && newline=1
-      printf "Resizing to ${res}p..."
-      convert "$name" -resize ${res}x${res}$c "$endname"
-      for k in {w,h}; do eval "$k=\$(identify -format \"%${k}\" \"$endname\")"; done
-      printf "Done!\n"; echo "Resized image: $name (${w}x${h})"
-    else
-      namer "$name"
-      cp "$name" "$endname"
+      #Apply to all
+      while true; do
+        read -p "Apply to all images? (y/n)" yn
+        case "$yn" in
+          [Yy] ) echo "s${res}" >> "$prefs"; break;;
+          [Nn] ) break;;
+          * ) echo "Please answer y/Y or n/N.";;
+        esac
+      done
     fi
 
   #Quiet (title+image)
   else
     for j in *.{png,jpg,jpeg}; do name="$j"; break; done
+    for k in {w,h}; do eval "$k=\$(identify -format \"%${k}\" \"$name\")"; done
+  fi
+
+  #Actual resize, either read from prefs or stored from above
+  if [[ $res ]]; then
+    (( w>h )) && c='^' || c=''
+    namer "${name%.*}.png" #Sets endname to non-taken filename
+    if [[ $global =~ ^I ]]; then
+      (( newline==0 )) && printf "\n" && newline=1
+      printf "Resizing to ${res}p..."
+    fi
+    convert "$name" -resize ${res}x${res}$c "$endname"
+    for k in {w,h}; do eval "$k=\$(identify -format \"%${k}\" \"$endname\")"; done
+    [[ $global =~ ^I ]] && printf "Done!\n" && echo "Resized image: $name (${w}x${h})"
+  else
     namer "$name"
     cp "$name" "$endname"
-    for k in {w,h}; do eval "$k=\$(identify -format \"%${k}\" \"$endname\")"; done
   fi
 
   #Bad dimension fix (BOTH MODES, but no msgs. in quiet)
@@ -210,7 +264,7 @@ makevid () {
     edit=0
     if ! grep -qs '^r' "$prefs"; then
       while true; do
-        read -p "Edit tracklist? Type 'd' to permanently hide this prompt. (y/n/d)" ynd
+        read -p "Edit tracklist? Type 'd' to disable prompt. (y/n/d)" ynd
         case "$ynd" in
           [Yy] ) edit=1; break;;
           [Nn] ) printf "\n"; break;;
@@ -251,13 +305,11 @@ makevid () {
               ;;
         [Nn] ) rm "$image"
               rm "$endname"
-              clear #Clean!
               return
               ;;
         * ) echo "Please answer y/Y or n/N.";;
       esac
     done
-    clear #Clean!
 
   #Quiet (audio)
   else
@@ -280,7 +332,7 @@ delve () {
 
 #Main
 for i in */; do
-  ( cd "$i"; [[ "$1" =~ ^-?[rR]$ ]] && delve || makevid )
+  ( cd "$i"; [[ $@ =~ [Rr] ]] && delve || makevid )
 done
 makevid
 
@@ -302,7 +354,7 @@ if [[ -e $titles ]]; then
 fi
 
 #Processing on fresh screen. Clear screen on interactive, regardless.
-[[ $global =~ ^I ]] && clear || printf "\n"
+printf "\n"
 
 #Actual makevid. Cycle through indices.
 for i in "${!titles_list[@]}"; do
